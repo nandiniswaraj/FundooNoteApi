2e3155157c1d6900439debc8508e53bb030d64e6
nandiniswaraj <nandiniswaraj1995@gmail.com>
2020-05-27 16:10:27 +0530

    [Nandiniswaraj]Add. all Main method file

---
+++ main/java/com/fundooproject/FundooBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fundooproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundooBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundooBackendProjectApplication.class, args);
+	}
+
+}
